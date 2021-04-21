a63b9a98ae65f237243d8f4b23de353eddbb5f4e
Allan Silva <allansilva956@gmail.com>
2021-04-20 20:42:40 -0400

    Added function calls on press button

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,28 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Correct Answer 1!');
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Flutter Course'),
-        ),
-        body: Text('This is my second flutter app.'),
-      ),
+          appBar: AppBar(
+            title: Text('Flutter Course'),
+          ),
+          body: Column(children: [
+            Text('This is my second flutter app.'),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () => print('Correct Answer 2')),
+            RaisedButton(
+                child: Text('Answer 3'),
+                onPressed: () {
+                  print('Correct Answer 3');
+                }),
+          ])),
     );
   }
 }
